987af9971a0398f32c22ab240a752385143e5d4d
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-05-12 01:46:28 +0200

    add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import { css } from '@pigment-css/react';
 import Slide1 from './Slide1';
 import { Template } from './Template';
 import { Backdrop } from './Backdrop';
-import { Heading, SlideLayout, Slide, Deck, Text, Image, Notes } from 'spectacle';
+import {
+  Heading,
+  SlideLayout,
+  UnorderedList,
+  ListItem,
+  Slide,
+  Deck,
+  Text,
+  Image,
+  Notes,
+} from 'spectacle';
 import { rawTheme } from '../rawTheme';
 
 const spectacleTheme = {
@@ -61,9 +71,9 @@ export default function Presentation() {
               solutions.
             </li>
             <li>
-              Now, React
-              Server Components might shake the space, but there is hope. I'm also going to introduce Pigment CSS, which is part of
-              a next generation movement for CSS-in-JS libraries.
+              Now, React Server Components might shake the space, but there is hope. I'm also going
+              to introduce Pigment CSS, which is part of a next generation movement for CSS-in-JS
+              libraries.
             </li>
           </ul>
         </Notes>
@@ -73,7 +83,8 @@ export default function Presentation() {
           A quick poll
         </Text>
         <Heading fontWeight="500" color="primary">
-          Who has styled-components<br/>
+          Who has styled-components
+          <br />
           or Emotion in their projet today?
         </Heading>
         <Notes>
@@ -93,14 +104,18 @@ export default function Presentation() {
           State of CSS
           <Image src="/images/stateofcss.svg" width="40" className={css({ marginLeft: 6 })} />
         </Text>
-        <Image src="/images/styled-components.png" width="80%" className={css({ marginTop: 16, transform: 'translate(20%)' })} />
+        <Image
+          src="/images/styled-components.png"
+          width="80%"
+          className={css({ marginTop: 16, transform: 'translate(20%)' })}
+        />
         <Notes>
           {/* 15s */}
           <ul>
             <li>You are not alone, a lot of people do.</li>
             <li>
-              According to the State of CSS survey, over 60% of
-              the frontend community uses one of these two.
+              According to the State of CSS survey, over 60% of the frontend community uses one of
+              these two.
             </li>
           </ul>
         </Notes>
@@ -110,7 +125,9 @@ export default function Presentation() {
           🌶️
         </Text>
         <Heading fontWeight="500" color="primary">
-          How feel left behind with them,<br/>considering migrating away?
+          How feel left behind with them,
+          <br />
+          considering migrating away?
         </Heading>
         <Notes>
           <ul>
@@ -118,6 +135,114 @@ export default function Presentation() {
             <li>How feel left behind with them, considering migrating away?</li>
             <li>🙆‍♂️</li>
             <li>I see xx% of the audience who raised their hands</li>
+          </ul>
+        </Notes>
+      </Slide>
+      <Slide>
+        {/* Source https://2023.stateofcss.com/en-US/css-in-js/ */}
+        <Text fontSize="2rem" color="secondary" className={css({ display: 'flex' })}>
+          State of CSS
+          <Image src="/images/stateofcss.svg" width="40" className={css({ marginLeft: 6 })} />
+        </Text>
+        <Image
+          src="/images/mood.png"
+          width="90%"
+          className={css({ margin: 'auto', marginBottom: 200 })}
+        />
+        <Notes>
+          {/* 15s */}
+          <ul>
+            <li>I see a good number of hand raised here! x% of the audience</li>
+            <li>
+              In the same State of CSS survey, you can see a clear decline of interest for Styled
+              components and Emotion
+            </li>
+            <li>What's going on?</li>
+          </ul>
+        </Notes>
+      </Slide>
+      <SlideLayout.Center>
+        {/* TODO: I took this from https://www.notion.so/mui-org/Logo-source-127ec595223047b19faaf9b4c4094ba8, but it's weird
+         It looks different */}
+        <Image
+          src="/images/MUI.svg"
+          width="400"
+          className={css({ margin: '0px auto 80px', })}
+        />
+        <div
+          className={css(({ theme }) => ({
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '1.2rem',
+            fontFamily: theme.typography.fontFamily,
+            fontSize: '1.5rem',
+            '> img': {
+              borderRadius: 999,
+              border: '4px solid',
+              borderColor: 'hsla(210 100% 50% / 0.3)',
+            },
+          }))}
+        >
+          <img src="/images/olivier.jpeg" width="70" height="70" />
+          <div
+            className={css({
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '0.2rem',
+            })}
+          >
+            <p className={css({ margin: 0, fontWeight: 500 })}>Olivier Tassinari</p>
+            <p className={css({ margin: 0, opacity: 0.5 })}>Co-creator of Material UI and CEO @MUI</p>
+          </div>
+        </div>
+        <Notes>
+          {/* 15s */}
+          <ul>
+            <li>I won't provide a definitive answer, but at MUI, we are facing the same challenges.</li>
+            <li>
+              co-creator of Material UI and CEO of MUI.
+            </li>
+          </ul>
+        </Notes>
+      </SlideLayout.Center>
+      <Slide>
+        <Text fontSize="2rem" color="secondary">
+          MUI
+        </Text>
+        <Heading fontWeight="500" color="primary">
+          We funds the development of:
+        </Heading>
+        <UnorderedList>
+          <ListItem>
+            <Text lineHeight={0.7}>Material UI</Text>
+          </ListItem>
+          <ListItem>
+            <Text lineHeight={0.7}>MUI X</Text>
+          </ListItem>
+          <ListItem>
+            <Text lineHeight={0.7}>Base UI</Text>
+          </ListItem>
+          <ListItem>
+            <Text lineHeight={0.7}>Toolpad</Text>
+          </ListItem>
+        </UnorderedList>
+        <Notes>
+          {/* 15s */}
+          <ul>
+            <li>
+              At MUI, we are facing the same challenges.
+              co-creator of Material UI and CEO of MUI.
+            </li>
+            <li>
+              For those of you that don't know us. MUI is the organization that funds the
+              development of multiple open-source projects in the React space: - Material UI, a
+              popular UI library that implements the Material Design principles. - MUI X, a set of
+              advanced React components like data grids and charts. - Base UI, Headless UI
+              components - Toolpad, a set of components to quickly build internal tools.
+            </li>
+            <li>For all of these projects, we rely on Emotion, one way or another.</li>
+            <li>And we are facing two main challenges</li>
           </ul>
         </Notes>
       </Slide>
