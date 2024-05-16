5f257970bc04898e84a98810632d93f4c9c4af3d
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-05-16 12:13:49 -0700

    add start of a benchmark

--- benchmark/src/app/page.tsx
+++ benchmark/src/app/page.tsx
@@ -1,93 +1,46 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+"use client";
+import * as React from 'react';
+import TableComponent from './table-component';
+import TableRaw from './table-raw';
+import TableSc from './table-sc';
+import TableMuiV5 from './table-mui-v5';
+import TableMuiV4 from './table-mui-v4';
+import TablePigment from './table-pigment';
+
+const componentMap = {
+  component: TableComponent,
+  raw: TableRaw,
+  sc: TableSc,
+  'mui-v4': TableMuiV4,
+  'mui-v5': TableMuiV5,
+  pigment: TablePigment,
+}
 
 export default function Home() {
+  const [child, setState] = React.useState<string | null>(null);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-        </a>
-      </div>
-    </main>
+    <div>
+      Render
+      <button onClick={() => setState('raw')}>
+        Raw
+      </button>
+      <button onClick={() => setState('component')}>
+        Component
+      </button>
+      <button onClick={() => setState('sc')}>
+        Styled-components
+      </button>
+      <button onClick={() => setState('mui-v4')}>
+        Material UI v4
+      </button>
+      <button onClick={() => setState('mui-v5')}>
+        Material UI v5
+      </button>
+      <button onClick={() => setState('pigment')}>
+        Pigment
+      </button>
+      {child !== null ? React.createElement(componentMap[child]) : null}
+    </div>
   );
 }
